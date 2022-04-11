afa1814a3dc27de943d896ab2a2269af1b139616
CarloChiodaroli <carlo.chiodaroli@mail.polimi.it>
2022-04-11 20:21:33 +0200

    Done CharacterCardFabric and StudentsContainer tests

---
+++ src/test/java/it/polimi/ingsw/model/phases/action/CharacterCardFabricTest.java
@@ -0,0 +1,67 @@
+package it.polimi.ingsw.model.phases.action;
+
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.phase.action.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Character Card Fabric")
+public class CharacterCardFabricTest {
+
+    @DisplayName("Test of character card's creation")
+    @ParameterizedTest
+    @EnumSource(Characters.class)
+    public void cardCreation(Characters character){
+        Game game = new Game();
+        game.addPlayer("Camilla");
+        game.addPlayer("Anja");
+        game.gameStarter();
+
+        CharacterCard card = CharacterCardFabric.createCard(character, game.getActionFase());
+
+        switch(Characters.getClassOfCard(character)){
+            case "StudentMovement" -> {
+                assertEquals(StudentMovementCard.class, card.getClass());
+                assertNotEquals(InfluenceCard.class, card.getClass());
+                assertNotEquals(MotherNatureCard.class, card.getClass());
+            }
+            case "MotherNature" -> {
+                assertNotEquals(StudentMovementCard.class, card.getClass());
+                assertNotEquals(InfluenceCard.class, card.getClass());
+                assertEquals(MotherNatureCard.class, card.getClass());
+            }
+            case "Influence" -> {
+                assertNotEquals(StudentMovementCard.class, card.getClass());
+                assertEquals(InfluenceCard.class, card.getClass());
+                assertNotEquals(MotherNatureCard.class, card.getClass());
+            }
+        }
+        assertEquals(character, card.getCharacter());
+    }
+
+    @DisplayName("Test of random card creation")
+    @RepeatedTest(30)
+    public void randomCardCreation(){
+        Game game = new Game();
+        game.addPlayer("Camilla");
+        game.addPlayer("Anja");
+        game.gameStarter();
+        List<CharacterCard> cards = CharacterCardFabric.getCards(game.getActionFase());
+
+        assertEquals(3, cards.size());
+
+        List<Characters> presentCharacters = cards.stream()
+                .map(CharacterCard::getCharacter)
+                .distinct()
+                .collect(Collectors.toList());
+        assertEquals(3, presentCharacters.size());
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/model/phases/action/StudentsContainerTest.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.model.phases.action;
+
+import it.polimi.ingsw.model.TeacherColor;
+import it.polimi.ingsw.model.phase.action.StudentsContainer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StudentsContainerTest {
+
+    @Test
+    public void test(){
+        StudentsContainer container = new StudentsContainer(4);
+
+        container.addStudent(TeacherColor.RED);
+        container.addStudent(TeacherColor.RED);
+        container.addStudent(TeacherColor.BLUE);
+        container.addStudent(TeacherColor.PINK);
+
+        assertEquals(2, container.howManyStudents(TeacherColor.RED));
+        assertEquals(1, container.howManyStudents(TeacherColor.BLUE));
+        assertEquals(1, container.howManyStudents(TeacherColor.PINK));
+        assertEquals(4, container.howManyTotStudents());
+
+    }
+}
