32b2dbb05a4c6564b0770c84816b51c91326afe9
Camilla Casiraghi <camilla.casiraghi@mail.polimi.it>
2022-04-14 09:45:13 +0200

    mother nature card testing

---
+++ src/test/java/it/polimi/ingsw/model/phases/action/MotherNatureCardTest.java
@@ -0,0 +1,54 @@
+package it.polimi.ingsw.model.phases.action;
+
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.phase.action.Characters;
+import it.polimi.ingsw.model.phase.action.MotherNatureCard;
+import it.polimi.ingsw.model.phase.action.MotherNatureState;
+import it.polimi.ingsw.model.table.Island;
+import it.polimi.ingsw.model.table.MotherNature;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MotherNatureCardTest {
+    @Test
+    public void motherNatureCardTest(){
+        Game game = new Game();
+        game.addPlayer("Camilla");
+        game.addPlayer("Anja");
+        game.gameStarter();
+        Characters messenger = Characters.MESSENGER;
+        MotherNatureCard motherNatureCard = new MotherNatureCard(messenger, game.getActionFase(),
+                Characters.getCharacterization(messenger));
+        MotherNatureState motherNatureState = new MotherNatureState(game.getActionFase());
+        motherNatureCard.activator(motherNatureState, game.getPlayers().get(0));
+        Optional<Island> initialpos = MotherNature.getMotherNature().getPosition();
+        Island inpos = initialpos.get();
+        int firstposition = game.getTable().getIslandList().indexOf(inpos);
+
+        motherNatureCard.handle(game.getPlayers().get(0), 6, 5);
+        Optional<Island> secondpos = MotherNature.getMotherNature().getPosition();
+        Island secpos = secondpos.get();
+        int secondposition = game.getTable().getIslandList().indexOf(secpos);
+        if (firstposition + 6 > 11)
+            assertEquals(firstposition - 12 + 6, secondposition);
+        else
+            assertEquals(firstposition + 6, secondposition);
+        firstposition = secondposition;
+
+        motherNatureCard.handle(game.getPlayers().get(0), 5, 3);
+        secondpos = MotherNature.getMotherNature().getPosition();
+        secpos = secondpos.get();
+        secondposition = game.getTable().getIslandList().indexOf(secpos);
+        if (firstposition + 5 > 11)
+            assertEquals(firstposition - 12 + 5, secondposition);
+        else
+            assertEquals(firstposition + 5, secondposition);
+
+    }
+
+}
