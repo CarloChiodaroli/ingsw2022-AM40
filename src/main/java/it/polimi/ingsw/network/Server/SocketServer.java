fda866aff2e5b1cb10360a2f59d6a1e3f0ec215d
Manuel_Colombo <manuel2.colombo@mail.polimi.it>
2022-05-01 13:26:16 +0200

    Client and server Socket

--- src/main/java/it/polimi/ingsw/network/Server/SocketServer.java
+++ src/main/java/it/polimi/ingsw/network/Server/SocketServer.java
@@ -2,33 +2,64 @@ package it.polimi.ingsw.network.Server;
 
 import it.polimi.ingsw.network.Message.Message;
 
+import javax.swing.plaf.TableHeaderUI;
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * It handles all the new socket connection.
  * It is a socket and there is installed the server
  */
-public class SocketServer {
-    //private final Server server;
+public class SocketServer  implements  Runnable{
     private final int port;
+    private final Server server;
     ServerSocket serverSocket;
 
-    public SocketServer(/*Server server,*/ int port) {
-        //this.server = server;
-        this.port = port;
+    public SocketServer(Server server,int port)
+    {
+        this.server=server;
+        this.port=port;
     }
 
-    public void addClient(String nickname, ClientHandler clientHandler)
+    public void run()
     {
-    //server adds client
+        try
+        {
+        serverSocket = new ServerSocket(port);
+        }catch (IOException e) {
+            return;
+        }
+        while (!Thread.currentThread().isInterrupted())
+        {
+            try {
+                Socket client = serverSocket.accept();
+                client.setSoTimeout(5000); // Timer that lunch error if the method doesn't finish
+                SocketClientHandler clientHandler = new SocketClientHandler(this,client);
+
+                Thread thread = new Thread(clientHandler,"Handler : "+client.getInetAddress());
+                thread.start();
+
+            }catch (IOException e)
+            {
+
+            }
+        }
+    }
+
+
+    public void addClient(String playerName,ClientHandler clientHandler)
+    {
+        server.addClient(playerName,clientHandler);
     }
 
     public void onMessageReceived(Message message)
     {
-        //server receives message
+        server.onMessageReceived();
     }
 
-    public void onDisconnect(ClientHandler clientHandler) {
-        //server disconnects
+    public void onDisconnect(ClientHandler clientHandler)
+    {
+        server.onDisconnect(clientHandler);
     }
 }
